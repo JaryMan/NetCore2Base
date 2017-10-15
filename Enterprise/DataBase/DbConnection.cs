a3d6c7e9e827ffe97f8b31ce3e8877b882ba2be2
Jary <574913627@qq.com>
2017-10-15 15:14:57 +0800

    11111111111111

---
+++ Enterprise/DataBase/DbConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enterprise.DataBase
+{
+    public partial class DbConnection : IDisposable
+    {
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public static string ConnectionString = string.Empty;
+        private SqlConnection _connection;
+        public SqlConnection connection => _connection ?? (_connection = GetOpenConnection());
+
+        public static SqlConnection GetOpenConnection(bool mars = false)
+        {
+            var cs = ConnectionString;
+            if (mars)
+            {
+                var scsb = new SqlConnectionStringBuilder(cs)
+                {
+                    MultipleActiveResultSets = true
+                };
+                cs = scsb.ConnectionString;
+            }
+            var connection = new SqlConnection(cs);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlConnection GetClosedConnection()
+        {
+            var conn = new SqlConnection(ConnectionString);
+            if (conn.State != ConnectionState.Closed) throw new InvalidOperationException("should be closed!");
+            return conn;
+        }
+
+        public void Dispose()
+        {
+            connection?.Close();
+            connection?.Dispose();
+        }
+    }
+}
---
+++ Enterprise/DataBase/SelectParameter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Enterprise.DataBase
+{
+    /// <summary>
+    /// 查询参数
+    /// </summary>
+    public class SelectParameter
+    {
+        /// <summary>
+        /// 页面索引
+        /// </summary>
+        public int PageIndex;
+
+        /// <summary>
+        /// 页面数量
+        /// </summary>
+        public int PageCount;
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string OrderByValue;
+
+        /// <summary>
+        /// 排序方式(asc正序,desc倒序)
+        /// </summary>
+        public string OrderBy;
+
+        public SelectParameter()
+        {
+            OrderByValue = "Id";
+            OrderBy = "asc";
+        }
+
+        public SelectParameter(string orderbyValue,string orderBy)
+        {
+            this.OrderByValue = orderbyValue;
+            this.OrderBy = orderBy;
+        }
+
+        public SelectParameter(string orderbyValue, string orderBy,int pageIndex,int pageCount)
+        {
+            this.OrderByValue = orderbyValue;
+            this.OrderBy = orderBy;
+            this.PageIndex = pageIndex;
+            this.PageCount = pageCount;
+        }
+
+    }
+}
